7e2f5aa51911be004c800520b196308709254c79
jinyanghe <839380338@qq.com>
2023-12-12 22:17:42 +0800

    - 完成提示功能，完成输入行解析功能

--- src/main/java/al2csv.java
+++ src/main/java/al2csv.java
@@ -1,17 +1,94 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Objects;
+import static java.lang.Integer.parseInt;
 
 public class al2csv {
     public static void main(String[] args) {
-        try{
-            ArrayList<String> arguments = parseString.parse();
-        }catch (IOException e) {
-            return;
-        }
-        /* pattern:
-        *  al2csv [-h] [-c/-e] [-rg] [[file] [file][...]] [-depth depth] [-r] [-d/-ad dir]
-        *
-        * */
+        helpmsg = "Welcome to al2csv, a commandline tool to help manage excel items and csv,\n" +
+                "BASIC USAGE: al2csv [-h]elp [-ctoe]xcel [-etoc]sv [-rg]ex [[file] [file][...]] [-depth depth] [-r]ecursive \n"+
+                "options:\n"+"-h: show this help message and exit.\n" +
+                "-ctoe, -etoc: transform csv files to excel files, or vise versa.\n" +
+                "-rg: enables regex pattern matching.\n"+
+                "-depth: assign limit to recursive searching when having subfolders.\n" +
+                "-r: apply recursive search to all subfolders.";
 
+        while(true){
+            try{
+                ArrayList<String> arguments = parseString.parse();
+                /* pattern:
+                 *  al2csv [-h] [ctoe/etoc] [-rg] [[file] [file][...]] [-depth depth] [-r]
+                 *
+                 * */
+                /*check sequence: -h? -etoc/-ctoe? -rg? how many files?(makeFileList) depth? -r? -d?
+                 */
+                boolean help = false; boolean etoc = false; boolean ctoe = false;
+                boolean regex = false; int depth = 1; boolean assignDepth = false;
+                boolean recursive = false;
+                if(!Objects.equals(arguments.get(0), "al2csv")){parseString.syntaxError();continue;}
+                arguments.remove("al2csv");
+                if(arguments.contains("-h")){
+                    if(arguments.size()!=2){parseString.syntaxError();continue;}
+                    System.out.println(helpmsg);
+                    continue;
+                } else if (arguments.contains("-etoc")) {
+                    etoc = true;
+                    arguments.remove("-etoc");
+                }
+                if(arguments.contains("-ctoe")){
+                    ctoe = true;
+                    arguments.remove("-ctoe");
+                    if(etoc) {
+                        parseString.syntaxError("ce");continue;}
+                }
+                if(arguments.contains("-rg")){
+                    regex = true;
+                    arguments.remove("-rg");
+                }
+                if(arguments.contains("-depth")){
+                    assignDepth = true;
+                    if(!arguments.get(arguments.indexOf("-depth")+1).matches("-?\\\\d+(\\\\.\\\\d+)?")){
+                        parseString.syntaxError("notdepth");
+                        continue;
+                    }
+                    if(arguments.get(arguments.indexOf("-depth")+1).contains(".")){
+                        parseString.syntaxError("notdepth");
+                        continue;
+                    }
+                    if(parseInt(arguments.get(arguments.indexOf("-depth")+1))<0){
+                        parseString.syntaxError("notdepth");
+                        continue;
+                    }
+                    depth = parseInt(arguments.get(arguments.indexOf("-depth")+1));
+                    arguments.remove("-depth");
+                    arguments.remove(Integer.toString(depth));
+                }
+                if(arguments.contains("-r")){
+                    recursive = true;
+                    arguments.remove("-r");
+                    if(assignDepth){parseString.syntaxError("dr");continue;}
+                }
+                ArrayList<File> FileList = new ArrayList<>();
+                ArrayList<String> reList = new ArrayList<>();
+                File curFile = new File(System.getProperty("user.dir"));
+                if(curFile.list())
+                if (regex){
+                    for(int i = 0; i < arguments.size(); i++){
+                        if(Objects.equals(arguments.get(i),"-depth")){i++;continue;}
+                        if(Objects.equals(arguments.get(i),"-r"))continue;
+                        reList.add(arguments.get(i));
+                    }
+                    for(String s :reList){
+                        for(File f : curFile.listFiles())
+                    }
+                }
+
+
+            }catch (IOException e) {
+                return;
+            }
+        }
     }
+
 }
--- src/main/java/parseString.java
+++ src/main/java/parseString.java
@@ -10,6 +10,27 @@ public class parseString {
         //al2csv -c [-d <dir>] [-rg <regex>] [|]
         return new ArrayList<>(Arrays.asList(str.split(" ")));
     }
+    public static void syntaxError(String cases){
+        switch (cases){
+            case "ce":
+            {
+                System.out.println("illegal arguments. you can only choose one file transform.)");
+            return;
+            }
+            case "dr":
+            {
+                System.out.println("cannot assign depth and use recursive search.");
+            return;
+            }
+            case "notdepth":
+            {
+                System.out.println("depth must be positive integer.");
+                return;
+            }
+        }
 
-
+    }
+    public static void syntaxError(){
+        System.out.println("illegal argument. maybe you want to use -h for help?");
+    }
 }
